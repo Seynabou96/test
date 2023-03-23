40b31a260ce8be16f281b41dbcd23a36d1edb9a0
Seynabou Niang <92727101+Seynabou96@users.noreply.github.com>
2023-03-23 12:15:59 -0700

    Update main.go

--- main.go
+++ main.go
@@ -39,11 +39,22 @@ func IsAlpha(s string) bool {
 	}
 	return true
 }
+func IsAWorld(s string) bool {
+	front := []string{"(cap)", "(low)", "(up)", "(low,", "(cap,", "(up,", "(hex)", "(bin)"}
+	// index:= Index(tab,s)
+	for i := 0; i < len(front); i++ {
+		if s == front[i] {
+			return false
+		}
+	}
+
+	return true
+}
 
 func Permutation(s []string) []string {
 
 	for i := 1; i < len(s); i++ {
-		a := []string{"(cap)", "(low)", "(up)"}
+		a := []string{"(cap)", "(low)", "(up)", "(low,"}
 		p := []string{",", ".", ":"}
 		// if i == 0{}
 		if Contains(p, s[i]) && Contains(a, s[i-1]) {
@@ -52,6 +63,13 @@ func Permutation(s []string) []string {
 			s[i-1] = tampon
 
 			// fmt.Println(s, 0)
+			return Permutation(s)
+		}
+		if Contains(p, s[i]) && s[i-2] == "(low," {
+			tampon := s[i]
+			s[i] = s[i-1]
+			s[i-1] = tampon
+
 			return Permutation(s)
 		}
 	}
@@ -98,15 +116,7 @@ func IsNumeric(s string) bool {
 	}
 	return true
 }
-func RespectTheFormat(a string) bool {
-	number, _ := strconv.Atoi(a)
-	numToString := strconv.Itoa(number)
-	b := numToString + ")"
-	if a == b {
-		return true
-	}
-	return false
-}
+
 func Index(s []string, e string) int {
 	for i, v := range s {
 		if v == e {
@@ -116,66 +126,16 @@ func Index(s []string, e string) int {
 	}
 	return -1
 }
-
-func TryIt(a []string) []string {
-	var result []string
-	front := []string{"(cap)", "(low)", "(up)", "(low,", "(cap,", "(up,", "(hex)", "(bin)"}
-
-	for i := 0; i < len(a); i++ {
-		if Contains(front, a[i]) && i == 0 {
-			// fmt.Println(a[i], i, i-1, 1)(a[i] == "(cap)" || a[i] == "(low)" || a[i] == "(up)")
-
-		} else if Contains(front, a[i]) && Contains(front, a[i-1]) && i == 1 {
-			// fmt.Println(a[i], i, i-1, 2)
-			// // fmt.Println(a[i], a[i-1])
-			// fmt.Println(a[i], a[i-1], result)
-
-		} else if Contains(front, a[i]) && Contains(front, a[i-1]) && i == 2 {
-			// fmt.Println(a[i], i, i-1, 2)
-			// // fmt.Println(a[i], a[i-1])
-			// fmt.Println(a[i], a[i-1], result)
-
-		} else if a[i] == "(cap)" {
-			// a[i-1] = Capitalise(a[i-1])
-			fmt.Println(a[i], i, i-1, 3)
-			fmt.Println(a[i], a[i-1], result)
-			fmt.Println(result[len(result)-1])
-
-			index := Index(result, result[len(result)-1])
-			result[index] = Capitalise(result[index])
-
-		} else if a[i] == "(low)" {
-			index := Index(result, result[len(result)-1])
-			fmt.Println(result[len(result)-1])
-			result[index] = strings.ToLower(result[index])
-			fmt.Println(a[i], a[i-1], result)
-
-		} else if a[i] == "(up)" {
-			index := Index(result, result[len(result)-1])
-			fmt.Println(result[len(result)-1])
-
-			result[index] = strings.ToUpper(result[index])
-			fmt.Println(a[i], a[i-1], result)
-
-		} else if a[i] == "(low," && i != len(a)-1 {
-
-			// number, _ := strconv.Atoi(a[i+1])
-			number := Trimatoi(a[i+1])
-			numToString := strconv.Itoa(number)
-			b := numToString + ")"
-			fmt.Println(a[i+1], number, numToString, b)
-			if a[i+1] == b {
-				fmt.Println("i found you")
-			} else {
-				fmt.Println("not found")
-
-			}
-
-		} else {
-			result = append(result, a[i])
+func FindAnAlphaNumericalWord(s []string, e string) bool {
+	// trouver l'index du mot
+	index := Index(s, e)
+	for i := 0; i < index; i++ {
+		//appeler la fonction alphanumérique
+		if !IsAWorld(s[i]) {
+			return true
 		}
 	}
-	return result
+	return false
 }
 
 func Capitalise(s string) string {
@@ -201,19 +161,103 @@ func Capitalise(s string) string {
 	return string(so)
 }
 
+func VerifyFormat(s string) bool {
+	// number, _ := strconv.Atoi(a[i+1]) (string, int)
+	number := Trimatoi(s)
+	//string du nombre
+	numToString := strconv.Itoa(number)
+	b := numToString + ")"
+
+	return b == s
+
+}
+
+func TryIt(a []string) []string {
+	var result []string
+	front := []string{"(cap)", "(low)", "(up)", "(low,", "(cap,", "(up,", "(hex)", "(bin)"}
+
+	for i := 0; i < len(a); i++ {
+		if Contains(front, a[i]) && FindAnAlphaNumericalWord(a, a[i]) {
+			// fmt.Println("i'am")
+
+		} else if a[i] == "(cap)" {
+			// a[i-1] = Capitalise(a[i-1])
+			// fmt.Println(a[i], i, i-1, 3)
+			// fmt.Println(a[i], a[i-1], result)
+			fmt.Println(result[len(result)-1])
+
+			index := Index(result, result[len(result)-1])
+			result[index] = Capitalise(result[index])
+
+		} else if a[i] == "(low)" {
+			index := Index(result, result[len(result)-1])
+			// fmt.Println(result[len(result)-1])
+			result[index] = strings.ToLower(result[index])
+			// fmt.Println(a[i], a[i-1], result)
+
+		} else if a[i] == "(up)" {
+			index := Index(result, result[len(result)-1])
+			result[index] = strings.ToUpper(result[index])
+			// fmt.Println(result[len(result)-1])
+
+			// fmt.Println(a[i], a[i-1], result)
+
+		} else if a[i] == "(low," && i != len(a)-1 {
+			// number, _ := strconv.Atoi(a[i+1]) (string, int)
+			number := Trimatoi(a[i+1])
+			//string du nombre
+			numToString := strconv.Itoa(number)
+			b := numToString + ")"
+			// fmt.Println(a[i+1], number, numToString, b)
+			if a[i+1] == b {
+
+				index := Index(result, result[len(result)-1])
+				// fmt.Println(result[index], index)
+				// fmt.Println(result)
+
+				//string du nombre
+				// numToString := strconv.Itoa(number)
+				if number > 0 {
+					if number < len(result) {
+						for i := index; i >= len(result)-number; i-- {
+							result[i] = strings.ToLower(result[i])
+
+						}
+					} else if number >= len(result) {
+						for i := index; i >= 0; i-- {
+							result[i] = strings.ToLower(result[i])
+
+						}
+					}
+				}
+				num := 2023
+				a[i+1] = string(rune(num))
+				// a = append(a[:i+1], a[i+2:]...)
+				// mois = append(mois[:indexASupprimer], mois[(indexASupprimer+1):]...)
+			}
+		} else if (a[i] == "(low," || a[i] == "(cap," || a[i] == "(up,") && i == len(a)-1 {
+		} else if a[i] == "ߧ" {
+
+		} else {
+			result = append(result, a[i])
+		}
+	}
+	return result
+}
+
 func main() {
-	s := []string{"i", "try", "(low,", "2)", "(low,", "3)3"}
+	// s := []string{"I", "TRY", "(low,", "2)", "(low,", "3)3"}
 	// s := []string{",", "(cap)", "ect", ".", "try", "(up)", ".", "(low)", ".", "(cap)", "ect", ",", "basta", ",", "(cap)"}
 
-	// s := []string{"(cap)", "(low)", "(up)", "ect", ".", "try", "(up)", ".", "(cap)", ".", "(low)", "ect", ",", "basta", ",", "(cap)"}
-	fmt.Println(s)
+	s := []string{"(low,", "(cap)", "(low)", "(up)", "ECT", ".", "try", "(up)", "(low,", "2)", ".", "(cap)", ".", "(up)", "eCt", ",", "baSta", ",", "(up)", "(low,", "2)", "(up)", "(low,", "(low,"}
+	// fmt.Println(s)
 
 	s = Permutation(s)
-	fmt.Println(s)
+	// fmt.Println(s)
 	f := strings.Join(s, " ")
-	fmt.Println(f)
+	// fmt.Println(f)
 	f = ApplyChangeForPermutations(f)
-	fmt.Println(f)
+	// fmt.Println(f)
 	s = strings.Fields(f)
 	fmt.Println(s)
 
